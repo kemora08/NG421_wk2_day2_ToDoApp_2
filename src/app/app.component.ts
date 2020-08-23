780c08ee6d065e204937ebc8af090e2c8ccfa3a6
kemora08 <kevin_mora89@hotmail.com>
2020-08-23 17:07:11 -0500

    app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,33 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Todos';
   todoList: any [] = [];
   todoTitle: string;
-  todoId: number = 0;
+  todoId = 0;
   ngOnInit() {
     this.todoTitle = '';
     this.todoList = [
       // example of how to make an item in todo list
       { title: 'Install Angular CLI', isDone: false },
-    
+
     ];
   }
-  addTodo():void {
+  addTodo(): void {
     this.todoList.push({
       title: this.todoTitle,
       isDone: false
     });
-    
+
     // resets our todoTitle variable to an empty string
     this.todoTitle = '';
   }
-  deleteTodo(todo:any) {
+  deleteTodo(todo: any) {
     const index = this.todoList.findIndex(todoItem => todoItem === todo);
     this.todoList.splice(index, 1);
   }
